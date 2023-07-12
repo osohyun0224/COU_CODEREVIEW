c2f96116cc6e763a29e25eab9c891d0d906d2475
osohyun0224 <osohyun0224@naver.com>
2023-07-12 17:24:42 +0900

    feat: 야호~~ 코드 입력기 안에 줄 생긴다~~

---
+++ src/views/CodeInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="code-input">
+    <textarea v-model="code" @input="updateCode" class="code-textarea"></textarea>
+    <pre>{{ code }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      code: ''
+    }
+  },
+  methods: {
+    updateCode(event) {
+      this.code = event.target.value;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.code-input {
+  margin: 0 auto;
+  width: 80%;
+}
+
+.code-textarea {
+  width: 100%;
+  height: 200px;
+  margin-bottom: 10px;
+}
+</style>
